3e534f17c3e98294b82b0d7b096d15def9ada962
jianjian.li <jianjian.li@cootek.cn>
2018-09-06 17:00:00 +0800

    [python] bob problem solved

---
+++ python/bob/bob.py
@@ -0,0 +1,11 @@
+def hey(phrase):
+    phrase = phrase.strip()
+    if phrase == "":
+        return "Fine. Be that way!"
+    elif phrase.isupper():
+        if phrase.endswith("?"):
+            return "Calm down, I know what I'm doing!"
+        return "Whoa, chill out!"
+    elif phrase.endswith("?"):
+        return "Sure."
+    return "Whatever."
