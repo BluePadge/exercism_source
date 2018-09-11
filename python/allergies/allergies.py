12a16e41a178bd8f500145863e066d30d8d18aa3
jianjian.li <jianjian.li@cootek.cn>
2018-09-11 11:57:31 +0800

    [python] add solve method for allergy problem

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,11 +1,29 @@
+def list_index(score, rets):
+    if score <= 0:
+        return rets
+    index = 0
+    while 2 ** (index + 1) <= score:
+        index += 1
+    rets.append(index)
+    score -= 2 ** index
+    return list_index(score, rets)
+
 class Allergies(object):
 
     def __init__(self, score):
-        pass
+        self.score = score
+        self.items = ["eggs", "peanuts", "shellfish",
+                      "strawberries", "tomatoes", "chocolate", "pollen", "cats"]
 
     def is_allergic_to(self, item):
-        pass
+        return item in self.lst
 
     @property
     def lst(self):
-        pass
+        results = []
+        rets = list_index(self.score,[])
+        for index in rets:
+            if index < len(self.items):
+                results.append(self.items[index])
+        return results
+
